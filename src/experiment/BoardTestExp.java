6f737b708adc431c03dc1bf85687fe2841dea786
HSpeedT <77657351+HSpeedT@users.noreply.github.com>
2022-10-07 17:37:19 -0600

    Added other test cases, commented each one properly

--- src/experiment/BoardTestExp.java
+++ src/experiment/BoardTestExp.java
@@ -1,14 +1,92 @@
 package experiment;
 
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BoardTestExp {
 	TestBoard board;
-		public void setUp() {
-			
-			board = new TestBoard();
-		}
-	
-	private void testAdjacency() {
-		TestBoardCell cell = board.getCell(0,0);
-		
+
+	@Before
+	public void setUp() {
+		// board should create adjacency list
+		board = new TestBoard();
+	}
+
+	/*
+	 * Test adjacencies for several different locations, Test centers and edges
+	 */
+
+	@Test
+	public void testAdjacency() {
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
+		Assert.assertEquals(2, testList.size());
+	}
+
+	/*
+	 * Test targets with several rolls and start locations
+	 */
+	@Test
+	public void testTargetsNormal() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+	}
+
+	/*
+	 * Test targets with rooms to make sure they are in targets list
+	 */
+	@Test
+	public void testTargetsRoom() {
+		board.getCell(0, 1).setIsRoom(true);
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 1);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(2, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+	}
+
+	/*
+	 * Test targets with rooms to make sure they are in targets list
+	 */
+	@Test
+	public void testTargetsOccupied() {
+		board.getCell(1, 0).setOccupied(true);
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 1);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(2, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertFalse(targets.contains(board.getCell(1, 0)));
+	}
+
+	/*
+	 * Test with mixed targets, rooms and cells to make sure they are both in
+	 * targets list
+	 */
+	@Test
+	public void testTargetsMixed() {
+		board.getCell(0, 2).setOccupied(true);
+		board.getCell(1, 2).setIsRoom(true);
+		TestBoardCell cell = board.getCell(0, 3);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
 	}
 }
