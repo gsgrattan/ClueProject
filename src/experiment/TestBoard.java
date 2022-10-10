82fb7667c9a1dcd7c4506eab0fb546ddac2e4ecd
gsgratton <gsgrattan@gmail.com>
2022-10-09 20:18:22 -0600

    Pushed code from last commit, not sure why it didn't push

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -10,17 +10,42 @@ public class TestBoard {
 	private Set<TestBoardCell> targets;
 	private List<List<TestBoardCell>> board;
 
-	// Constructor
+	// Constructor, fills board with default TestBoardCell Objects
 	public TestBoard(int rows, int columns) {
+
 		targets = new HashSet<TestBoardCell>();
 		board = new ArrayList<List<TestBoardCell>>(rows);
 
 		for (int i = 0; i < rows; i++) {
 			ArrayList<TestBoardCell> r = new ArrayList<TestBoardCell>(columns);
 			for (int j = 0; j < columns; j++) {
-				r.add(new TestBoardCell());
+				r.add(new TestBoardCell(i, j));
 			}
 			this.board.add(r);
+		}
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < rows; j++) {
+				if (i - 1 >= 0) {
+					TestBoardCell adj = this.board.get(i - 1).get(j);
+					this.board.get(i).get(j).addAdjacency(adj);
+				}
+
+				if (i + 1 < rows) {
+					TestBoardCell adj = this.board.get(i + 1).get(j);
+					this.board.get(i).get(j).addAdjacency(adj);
+				}
+
+				if (j - 1 >= 0) {
+					TestBoardCell adj = this.board.get(i).get(j - 1);
+					this.board.get(i).get(j).addAdjacency(adj);
+				}
+
+				if (j + 1 < rows) {
+					TestBoardCell adj = this.board.get(i).get(j + 1);
+					this.board.get(i).get(j).addAdjacency(adj);
+				}
+			}
 		}
 	}
 
@@ -31,6 +56,25 @@ public class TestBoard {
 
 	// Calculates all the targets and stores them in the targets set
 	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		this.calcTargets(startCell, pathlength, new HashSet<>());
+	}
+
+	private void calcTargets(TestBoardCell startCell, int pathlength, Set<TestBoardCell> visited) {
+		if (visited.isEmpty()) {
+			visited.add(startCell);
+		}
+
+		if (pathlength == 0 || startCell.getRoom()) {
+			targets.add(startCell);
+		} else {
+			for (TestBoardCell cell : startCell.getAdjList()) {
+				if (!cell.getOccupied() && !visited.contains(cell)) {
+					visited.add(cell);
+					calcTargets(cell, pathlength - 1, visited);
+					visited.remove(cell);
+				}
+			}
+		}
 	}
 
 	// return the targets list
