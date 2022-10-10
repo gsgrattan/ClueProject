aab57e8e300747821946d30009a46e2f751e33ab
Hunter S. Thompson <hunter@smallworks.com>
2022-10-09 18:47:47 -0600

    TestBoard constructor now takes rows, columns as its inputs (for how big the board is). Constructs an arraylist of default cells of size mxn. getCell now returns the proper cell from the board rather than an empty cell.

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,20 +1,32 @@
 package experiment;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class TestBoard {
 
 	private Set<TestBoardCell> targets;
+	private List<List<TestBoardCell>> board;
 
 	// Constructor
-	public TestBoard() {
+	public TestBoard(int rows, int columns) {
 		targets = new HashSet<TestBoardCell>();
+		board = new ArrayList<List<TestBoardCell>>(rows);
+
+		for (int i = 0; i < rows; i++) {
+			ArrayList<TestBoardCell> r = new ArrayList<TestBoardCell>(columns);
+			for (int j = 0; j < columns; j++) {
+				r.add(new TestBoardCell());
+			}
+			this.board.add(r);
+		}
 	}
 
 	// Return a specific cell from the targets I assume.
 	public TestBoardCell getCell(int row, int column) {
-		return new TestBoardCell(row, column);
+		return board.get(row).get(column);
 	}
 
 	// Calculates all the targets and stores them in the targets set
