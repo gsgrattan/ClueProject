39645e7720011df7730c015fa8a22c1b87b69a6f
Hunter S. Thompson <hunter@smallworks.com>
2022-10-11 14:21:57 -0600

    Filled out method stubs

--- src/clueGame/Room.java
+++ src/clueGame/Room.java
@@ -1,20 +1,39 @@
 package clueGame;
 
 public class Room {
+	private String name;
+	private BoardCell center;
+	private BoardCell labelCell;
 
-	public Object getName() {
+	public Room(String name) {
+		this.name = name;
+	}
+
+	public BoardCell getCenter() {
+		return center;
+	}
+
+	public void setCenter(BoardCell center) {
+		this.center = center;
+	}
+
+	public void setLabelCell(BoardCell labelCell) {
+		this.labelCell = labelCell;
+	}
+
+	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
 	public BoardCell getLabelCell() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.labelCell;
 	}
 
 	public BoardCell getCenterCell() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.center;
 	}
 
 }
